bf334cf69d4ce5f2c3141a56ecbf59d47e9e578a
RoySamson-stack <rsam9009@gmail.com>
2021-09-02 22:03:26 +0300

    making the a slice method to separate the year and century

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-var gender = ["male", "female"];
-var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let gender = ["male", "female"];
+let day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+// let maleNames=["Kwasi", "Kwadwo", "Kwabena","Kwaku", "Yaw", "Kofi","Kwame"];
+// let femaleNames=["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
-function dayCalculator(){
 var date = document.getElementById('date');
 var month = document.getElementById('month');
 var year = document.getElementById('year');
 var CC =  year.slice(0, -2);
 var YY = year.splice(2);
 
+
+function nameGiver(){
+  if(gender === "male" && day === "Sunday"){
+    console.log("Kwasi")
+  }else if(){
+
+  }
 }
