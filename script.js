4fb1a634b486839de7d45bd9f169484999fa39ce
RoySamson-stack <rsam9009@gmail.com>
2021-09-02 14:10:42 +0300

    making the function to calculate the day of birth

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 var gender = ["male", "female"];
 var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+function dayCalculator(){
+var date = document.getElementById('date');
+var month = document.getElementById('month');
+var year = document.getElementById('year');
+var CC =  year.slice(0, -2);
+var YY = year.splice(2);
+
+}
