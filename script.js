abf1cdb1c02031dca964421806696c1e7e7198c0
RoySamson-stack <rsam9009@gmail.com>
2021-09-03 11:46:49 +0300

    making the options for the genre in javascript

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 let gender = ["male", "female"];
 let day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-// let maleNames=["Kwasi", "Kwadwo", "Kwabena","Kwaku", "Yaw", "Kofi","Kwame"];
-// let femaleNames=["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+let maleNames=["Kwasi", "Kwadwo", "Kwabena","Kwaku", "Yaw", "Kofi","Kwame"];
+let femaleNames=["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
-var date = document.getElementById('date');
-var month = document.getElementById('month');
-var year = document.getElementById('year');
+
+function dayOfBirth(){
+  var date = document.getElementById('date').value;
+var month = document.getElementById('month').value;
+var year = document.getElementById('year').value;
 var CC =  year.slice(0, -2);
-var YY = year.splice(2);
 
+  var theDay = ( ( (CC/4) -2*CC-1) + ((5*year/4) ) + ((26*month+1)/10)) + date)%7;
+
+  var genders = document.getElementById('genders');
+  var gender = gender.options[genders.selectedIndex].value
+
+  let day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let maleNames=["Kwasi", "Kwadwo", "Kwabena","Kwaku", "Yaw", "Kofi","Kwame"];
+let femaleNames=["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+}
 
 function nameGiver(){
   if(gender === "male" && day === "Sunday"){
