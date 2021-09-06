679ca392db47ff96547f29cef94ee4ed8ee6fffb
RoySamson-stack <rsam9009@gmail.com>
2021-09-06 10:27:22 +0300

    fixing the script to show accurate date, days and names according to the calculation

--- script.js
+++ script.js
@@ -1,69 +1,29 @@
-var date = document.getElementById('date').value;
-var month = document.getElementById('month').value;
-var year = document.getElementById('year').value;
-var CC = year.toString().slice(0, -2);
-var YY = year.toString().slice(-2);
-
-var theDay = Math.floor((((CC / 4) - 2 * CC - 1) + (5 * YY / 4) + ((26 * month + 1) / 10) + date) % 7);
-var day = [null, "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
-var genders = document.getElementById('sex');
-var gender = genders.options[genders.selectedIndex].text;
-
-var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-
-var result = document.getElementById('results');
-
 
 function dayOfBirth() {
- 
-
-  // if(date.checkValidity()){
-  //   alert("enter the date");
-  // }else if(month.checkValidity()){
-  //  alert("Enter the month");
-  // }else if(year.checkValidity()){
-  //   alert("Enter the year");
-  // }
-
-  if (gender === "Male") {
-    result.innerText = "Born on " + day[theDay] + " so your name is " + maleNames[0];
-  } else if (gender === "Female") {
-    result.innerText = "Born on " + day[theDay] + " so your name is " + femaleNames[0];
-  } else if (gender === "Male") {
-    result.innerText = "Born on " + day[theDay] + " so your name is" + maleNames[1];
-  } else if (gender === "Female") {
-    result.innerText = "Born on " + day[theDay] + " so your name is " + femaleNames[1];
-  } else if (gender === "Male") {
-    result.innerText = "Born on " + day[theDay] + " so your name is " + maleNames[2];
-  } else if (gender === "Female") {
-    result.innerText = "Born on " + day[theDay] + " so your name is " + femaleNames[2];
-  } else if (gender === "Male") {
-    result.innerText = "Born on " + day[theDay] + " so your name is " + maleNames[3];
-  } else if (gender === "Female") {
-    result.innerText = "Born on " + day[theDay] + " so your name is " + femaleNames[3];
-  } else if (gender === "Male") {
-    result.innerText = "Born on " + day[theDay] + " so your name is " + maleNames[4];
-  } else if (gender === "Female") {
-    result.innerText = "Born on " + day[theDay] + " so your name is " + femaleNames[4];
-  } else if (gender === "Male") {
-    result.innerText = "Born on " + day[theDay] + " so your name is " + maleNames[5];
-  } else if (gender === "Female") {
-    result.innerText = "Born on " + day[theDay] + " so your name is " + femaleNames[5];
-  } else if (gender === "Male") {
-    result.innerText = "Born on " + day[theDay] + " so your name is " + maleNames[6];
-  } else if (gender === "Female") {
-    result.innerText = "Born on " + day[theDay] + " so your name is " + femaleNames[6];
+  var date = document.getElementById('date').value;
+  var month = document.getElementById('month').value;
+  var year = document.getElementById('year').value;
+  var CC = year.slice(0,2);
+  var YY = year.slice(2, 4);
+  
+  var theDay = Math.floor((((CC / 4) - 2 * CC - 1) + (5 * YY / 4) + ((26 * month + 1) / 10) + date) % 7);
+  var day = [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  
+  var genders = document.getElementById('sex');
+  var gender = genders.options[genders.selectedIndex].text;
+  
+  var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+  var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+  
+  var results = document.getElementById('results');
+  
+    if (gender === "Male") {
+         results.innerText = "You were born on " + day[theDay - 1]+ " and your name is " +  maleNames[theDay -1];
+         }
+         else if (gender === "Female") {
+             results.innerText = "You were born on " + day[theDay - 1]+ " and your name is " +  femaleNames[theDay -1];
+         }
+        
   }
-}
 
-// function validate() {
-//   if (date.length = 2 && (date => 1 && date <= 31)) {
-//     alert("enter the date");
-//   } else if (month.length = 2 && (month => 1 && month <=12)) {
-//     alert("Enter the month");
-//   } else if (year.length = 4) {
-//     alert("Enter the year");
-//   }
-// }
+  
